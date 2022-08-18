db099863c989188d5291817f4b05c7d495fa24dd
Evan Dickinson <me@evand.name>
2022-08-18 11:37:46 -0700

    Implement search

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,37 @@ import StoreItemGallery from './StoreItemGallery';
 import invariant from 'invariant';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
   const [{data: apiData, loading, error}] = useAxios(TOP_ALBUMS_URL);
 
   const items: StoreItemData[] | undefined = React.useMemo(
-    () => apiData ? buildStoreItemData(apiData) : undefined,
-  [apiData]);
+    // () => apiData ? buildStoreItemData(apiData) : undefined,
+    () => {
+      if (!apiData) {
+        return undefined;
+      }
+
+      let items = buildStoreItemData(apiData);
+      if (searchTerm) {
+        const searchFields: Array<keyof StoreItemData> = [
+          'artist', 'category', 'name'
+        ]
+        items = items.filter(item => {
+          const valuesToSearch = searchFields.map(fieldName => {
+            const value = item[fieldName]
+            invariant(typeof value === 'string', 'Can only search on string fields');
+            return value.toLowerCase().normalize();
+          });
+          return valuesToSearch.some(value => value.includes(searchTerm))
+        })
+      }
+
+      return items;
+    },
+  [apiData, searchTerm]);
 
   if (loading) {
     return <>Loading</>;
@@ -27,6 +51,11 @@ export default function App() {
   return (
     <Box sx={{padding: 4}}>
       <Typography variant="h3" gutterBottom>Top 100 albums</Typography>
+      <TextField 
+        label="Search" 
+        variant="outlined"
+        onChange={(event) => setSearchTerm(event.target.value.toLowerCase().normalize())}
+      />
       <StoreItemGallery items={items} />
     </Box>
   );
@@ -57,6 +86,34 @@ It renders albums
 * for i in 0 to numItems:
   - Expect the ith StoreItem's name to equal the ith name from the mock JSON,
     verifying that the items are sorted by popularity.
+
+
+It searches by name
+====================
+* Using Axios, mock GET TOP_ALBUMS_URL to return the mock data found
+  in StoreItemData.mock.json
+* Mount the `App` component
+* Type "now" into the sarch box
+* Find all the StoreItem components
+* Expect there to be exactly 3:
+  - NOW That's What I Call Country, Vol. 15
+  - NOW That's What I Call Music! Vol. 83
+  - If I Know Me
+
+It searches by {category, artist}
+==================================
+* Like "It searches by name", but slightly different
+
+Search is case insensitive
+==========================
+* Repeat "it searches by name" with search strings of "NOW", "Now", and "nOw"
+* Note that the search data is already case-insensitive -- it finds "NOW" vs "know"
+
+Search handles Unicocde edge cases
+==================================
+TBH, Unicode edge cases -- like the various ways to form diacritics -- are something I
+need to look up whenever I work with them. I don't trust myself to remember all the
+little details.
 
 */
 
