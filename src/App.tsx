7b89455e737ff5f908da3fb3f98575c5265a35d1
Evan Dickinson <me@evand.name>
2022-08-20 12:38:56 -0700

    Enable sort by popularity

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,24 @@ import invariant from 'invariant';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import { MenuItem } from '@mui/material';
+import lodash from "lodash";
 
-function useFilteredItems(items: StoreItemData[] | undefined, searchTerm: string): StoreItemData[] | undefined {
+// We have to flatten the key and direction into one enum, because the
+// MenuItem component needs its value to be a primitive type. MenuItem
+// won't accept an object like {key: 'rank', direction: 'ascending'}.
+enum SortScheme {
+  RankAscending,
+  RankDescending,
+}
+
+function useFilteredItems(
+  items: StoreItemData[] | undefined, 
+  searchTerm: string,
+): StoreItemData[] | undefined {
   return React.useMemo(() => {
     if (!items) {
       return undefined;
@@ -20,7 +36,7 @@ function useFilteredItems(items: StoreItemData[] | undefined, searchTerm: string
     const searchFields: Array<keyof StoreItemData> = [
       'artist', 'category', 'name'
     ]
-    return items.filter(item => {
+    const filteredItems = items.filter(item => {
       const valuesToSearch = searchFields.map(fieldName => {
         const value = item[fieldName];
         invariant(typeof value === 'string', 'Can only search on string fields');
@@ -28,11 +44,47 @@ function useFilteredItems(items: StoreItemData[] | undefined, searchTerm: string
       });
       return valuesToSearch.some(value => value.includes(searchTerm))
     });
+
+    return filteredItems;
   }, [items, searchTerm]);
+}
+
+function useSortedItems(items: StoreItemData[] | undefined, sortScheme: SortScheme): StoreItemData[] | undefined {
+  return React.useMemo(() => {
+    if (!items) {
+      return undefined;
+    }
+
+    let sortKey: keyof StoreItemData;
+    switch (sortScheme) {
+      case SortScheme.RankAscending:
+      case SortScheme.RankDescending:
+        sortKey = 'rank';
+        break;
+    }
+
+    let isDescending: boolean;
+    switch (sortScheme) {
+      case SortScheme.RankAscending:
+        isDescending = false;
+        break;
+      case SortScheme.RankDescending:
+        isDescending = true;
+    }
+
+    const sortedItems = lodash.sortBy(items, [sortKey]);
+    if (isDescending) {
+      sortedItems.reverse();
+    }
+
+    return sortedItems
+  }, [items, sortScheme]);
 }
 
 export default function App() {
   const [searchTerm, setSearchTerm] = React.useState("");
+  const [sortScheme, setSortScheme] = React.useState(SortScheme.RankAscending);
+
   const [{data: apiData, loading, error}] = useAxios(TOP_ALBUMS_URL);
 
   const allItems = React.useMemo(() => {
@@ -43,6 +95,7 @@ export default function App() {
     }
   }, [apiData]);
   const filteredItems = useFilteredItems(allItems, searchTerm);
+  const filteredSortedItems = useSortedItems(filteredItems, sortScheme);
 
   if (loading) {
     return <>Loading</>;
@@ -53,7 +106,7 @@ export default function App() {
     return <>Error</>;
   }
 
-  invariant(allItems !== undefined && filteredItems !== undefined, "If `loading` and `error` are falsy, then `allItems` and `filteredItems` must be defined");
+  invariant(allItems !== undefined && filteredSortedItems !== undefined, "If `loading` and `error` are falsy, then `allItems` and `filteredSortedItems` must be defined");
   return (
     <Box sx={{padding: 4}}>
       <Typography variant="h3" gutterBottom>Top {allItems.length} albums</Typography>
@@ -62,13 +115,33 @@ export default function App() {
         variant="outlined"
         onChange={(event) => setSearchTerm(event.target.value.toLowerCase().normalize())}
       />
-      <StoreItemGallery items={filteredItems} />
+
+    <FormControl>
+      <InputLabel id="select-sort-by-label">Sort by</InputLabel>
+      <Select
+        labelId="select-sort-by-label"
+        id="select-sort-by"
+        value={sortScheme}
+        label="Sort by"
+        onChange={event => {
+          const newScheme = event.target.value;
+          // TS thinks that newScheme has type SortScheme | string
+          invariant(typeof newScheme !== 'string', "newScheme must be a SortScheme");
+          setSortScheme(newScheme);
+        }}
+      >
+        <MenuItem value={SortScheme.RankAscending}>Popularity, high to low</MenuItem>
+        <MenuItem value={SortScheme.RankDescending}>Popularity, low to high</MenuItem>
+      </Select>
+    </FormControl>
+
+      <StoreItemGallery items={filteredSortedItems} />
     </Box>
   );
 }
 
 /*
-Test cases
+Integration test cases for App component
 
 It renders loading, when no data is available
 =============================================
@@ -89,7 +162,7 @@ It renders albums
 * Mount the `App` component
 * Find all the StoreItem components
 * expect the count from StoreItemData.mock.json to equal the count of StoreItem components
-* for i in 0 to numItems:
+* for i of 0 to numItems:
   - Expect the ith StoreItem's name to equal the ith name from the mock JSON,
     verifying that the items are sorted by popularity.
 
@@ -138,5 +211,79 @@ TBH, Unicode edge cases -- like the various ways to form diacritics -- are somet
 need to look up whenever I work with them. I don't trust myself to remember all the
 little details.
 
+It sorts by popularity
+======================
+* Using Axios, mock GET TOP_ALBUMS_URL to return the mock data found
+  in StoreItemData.mock.json
+* Mount the `App` component
+* Find the "sort by" dropdown and change it to "Popularity, low to high"
+* // In this integration test, spot check the first and last items.
+  // Full sorting is handled in the unit tests
+* Expect first album displayed to be the last album in the JSON
+* Expect last album displayed to be the first album in the JSON
+* // Change back
+* Find the "sort by" dropdown and change it to "Popularity, high to low"
+* Expect first album displayed to be the first album in the JSON
+* Expect last album displayed to be the last album in the JSON
+
+It searches and filters
+=======================
+* * Using Axios, mock GET TOP_ALBUMS_URL to return the mock data found
+  in StoreItemData.mock.json
+* Mount the `App` component
+* Find the "sort by" dropdown and change it to "Popularity, low to high"
+* Search for "country"
+* Expect the first album shown to be "Country Stuff The Album"
+* Expect the last album shown to be "& - EP". Yes, that is really its name :-P
+
 */
 
+/*
+unit tests for useSortedItems
+
+Scaffolding
+===========
+The idea here is to create a minimal component that uses the useSortedItems hook,
+and makes it easier to verify the hook's output. Using the hook inside a component
+gives a more realistic example of its use, as compared to doing all the shenannigans
+required to call the hook directly in a unit test.
+
+interface Props {
+  items: items: StoreItemData[] | undefined;
+  itemKey: keyof StoreItemData;
+  sortScheme: SortScheme;
+}
+
+function SortedItems({items, itemKey, sortScheme}: Props): JSX.Element {
+  const sortedItems = useSortedItems(items, sortScheme);
+  if (!sortedItems) {
+    return <>No data</>;
+  }
+  return (<ol>
+    {sortedItems.map(item => <li>{item[itemKey]}</li>)};
+  </ol>)
+}
+
+It returns no data if there is no input
+=======================================
+* // key and sortScheme don't matter
+* mount <SortedItems items={undefined} itemKey={rank} sortScheme={SortScheme.RankAscending} />
+* expect not to find an <ol> element
+* expect to find a text element with "no data"
+
+It sorts by rank ascending
+==========================
+* const items = StoreItemData.mock.json
+* mount <SortedItems items={items} itemKey={rank} sortScheme={SortScheme.RankAscending} />
+* let prevRank = -Infinity
+* count all <li> elements; expect them to equal the count of items from the JSON
+* for each <li> element:
+  - const currRank = element's text, converted to number
+  - expect currRank to be greater than prevRank
+  - prevRank = currRank
+
+It sorts by rank descending
+===========================
+Follow the pattern in "rank ascending"
+
+*/
